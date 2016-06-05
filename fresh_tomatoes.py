aa8e8212aa2f8706691c2f2a469d6ab6f877d433
fishwang2016 <zhenyu2004@hotmail.com>
2016-06-05 20:14:15 +0800

    added instance, class, styles.

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,7 +1,7 @@
 import webbrowser
 import os
 import re
-
+import media
 
 # Styles and scripting for the page
 main_page_head = '''
@@ -10,53 +10,16 @@ main_page_head = '''
 <head>
     <meta charset="utf-8">
     <title>Fresh Tomatoes!</title>
-
+   
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+    
+     <link rel="stylesheet" href="css/styles.css">
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-    <style type="text/css" media="screen">
-        body {
-            padding-top: 80px;
-        }
-        #trailer .modal-dialog {
-            margin-top: 200px;
-            width: 640px;
-            height: 480px;
-        }
-        .hanging-close {
-            position: absolute;
-            top: -12px;
-            right: -12px;
-            z-index: 9001;
-        }
-        #trailer-video {
-            width: 100%;
-            height: 100%;
-        }
-        .movie-tile {
-            margin-bottom: 20px;
-            padding-top: 20px;
-        }
-        .movie-tile:hover {
-            background-color: #EEE;
-            cursor: pointer;
-        }
-        .scale-media {
-            padding-bottom: 56.25%;
-            position: relative;
-        }
-        .scale-media iframe {
-            border: none;
-            height: 100%;
-            position: absolute;
-            width: 100%;
-            left: 0;
-            top: 0;
-            background-color: white;
-        }
-    </style>
+   
+ 
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
         $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
@@ -165,3 +128,20 @@ def open_movies_page(movies):
     # open the output file in the browser (in a new tab, if possible)
     url = os.path.abspath(output_file.name)
     webbrowser.open('file://' + url, new=2)
+    
+showgirls = media.Movie("https://youtu.be/gszDLDFwcLk","Show Girls","https://thenypost.files.wordpress.com/2016/06/bechdel.jpg?quality=90&strip=all&w=664&h=441&crop=1")
+approach_unknow=media.Movie("https://youtu.be/CQx2eosUlXY","Approaching the Unknown","https://thenypost.files.wordpress.com/2016/06/m45.jpg?quality=90&strip=all&w=664&h=441&crop=1")
+time_to_choose=media.Movie("https://youtu.be/Xj6-oIyXM20","Time to Choose","https://thenypost.files.wordpress.com/2016/06/chinese_masks.jpg?quality=90&strip=all&w=664&h=441&crop=1")
+
+the_witness =media.Movie("https://youtu.be/gpU6UVfIQLM","The Witness","https://thenypost.files.wordpress.com/2016/06/kitty_genovese.jpg?quality=90&strip=all&w=664&h=441&crop=1")
+popstar = media.Movie("https://youtu.be/S207jSgL_jw","Popstar","https://thenypost.files.wordpress.com/2016/06/popstar-_never_stop_never_stopping_-2016.jpg?quality=90&strip=all&w=664&h=441&crop=1")
+me_before_you = media.Movie("https://youtu.be/Eh993__rOxA","Me Before You","https://thenypost.files.wordpress.com/2016/06/me_before_you.jpg?quality=90&strip=all&w=664&h=441&crop=1")
+
+monthly_report= media.Movie("https://youtu.be/cbw9hlBnG74","Minority Report","https://thenypost.files.wordpress.com/2016/05/adaptations.jpg?quality=90&strip=all&w=664&h=441&crop=1")
+alice_eyes = media.Movie("https://youtu.be/x3IWwnNe5mc","Alice Through the Looking Glass","https://thenypost.files.wordpress.com/2016/06/gettyimages-533878914.jpg?quality=90&strip=all&w=664&h=441&crop=1")
+top_gun =media.Movie("https://youtu.be/ioWpe3hdFH0","Top Gun","https://thenypost.files.wordpress.com/2016/05/topgun2a.jpg?quality=90&strip=all&strip=all")
+
+
+movies =[showgirls,approach_unknow,time_to_choose,the_witness,popstar,me_before_you,monthly_report,alice_eyes,top_gun]
+
+open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 05 19:20:29 2016
+
+@author: Fish
+"""
+
+class Movie():
+    
+    def __init__(self, trailer_youtube_url , title,poster_image_url):
+        self.trailer_youtube_url = trailer_youtube_url 
+        self.title = title
+        self.poster_image_url = poster_image_url
+        
+        
+        
+
+
+        
