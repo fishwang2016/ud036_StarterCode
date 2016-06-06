01ca1d5c0116447557563bdd0a9102cc4eb45755
fishwang2016 <zhenyu2004@hotmail.com>
2016-06-06 10:40:33 +0800

    added docstrings

--- media.py
+++ media.py
@@ -6,14 +6,21 @@ Created on Sun Jun 05 19:20:29 2016
 """
 
 class Movie():
+    """
+    This a Movie class for gouping all the information for a movie
+    And could add necessary methods
+    """
     
     def __init__(self, trailer_youtube_url , title,poster_image_url):
+        
+        """
+        This is the constructor .
+        It requires to input youtube trailer URL, movie name, poster image url.
+        It can be call using instance.* for each relevant element.
+        
+        """
         self.trailer_youtube_url = trailer_youtube_url 
         self.title = title
         self.poster_image_url = poster_image_url
         
-        
-        
-
-
         
